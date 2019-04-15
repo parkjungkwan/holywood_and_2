8d674dd24d9644e5a0eb1be4ee2273d657b8a87c
pakjkwan <pakjkwan@gmail.com>
2019-04-15 18:08:19 +0900

    first

---
+++ app/src/main/java/app/demo/com/contacts/Index.java
@@ -0,0 +1,13 @@
+package app.demo.com.contacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+    }
+}
