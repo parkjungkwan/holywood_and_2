1cb849d1550d3c445c429a1916b83e324480dda0
pakjkwan <pakjkwan@gmail.com>
2019-04-17 14:51:17 +0900

    14:51

--- app/src/main/java/app/demo/com/contacts/MemberDetail.java
+++ app/src/main/java/app/demo/com/contacts/MemberDetail.java
@@ -1,7 +1,16 @@
 package app.demo.com.contacts;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import static app.demo.com.contacts.Index.*;
 
 public class MemberDetail extends AppCompatActivity {
 
@@ -9,5 +18,127 @@ public class MemberDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_detail);
+        Context _this = MemberDetail.this;
+        Intent intent = this.getIntent();
+        String seq = intent.getExtras().getString("seq");
+        Toast.makeText(_this,"넘어온 시퀀스: "+seq,Toast.LENGTH_LONG).show();
+        ItemDetail query = new ItemDetail(_this);
+        query.id = seq;
+        Member m = (Member)new ISupplier() {
+            @Override
+            public Object get() {
+                return null;
+            }
+        }.get();
+        ImageView photo = findViewById(R.id.profile);
+        photo.setImageDrawable(
+                getResources()
+                        .getDrawable(
+                                getResources()
+                                        .getIdentifier(
+                                                _this.getPackageName()+":drawable/"
+                                                        +((String)new Index.ISupplier() {
+                                                    @Override
+                                                    public Object get() {
+                                                        return query.get();
+                                                    }
+                                                }.get()),
+                                                null,
+                                                null),
+                                _this.getTheme()
+                        )
+        );
+        TextView name = findViewById(R.id.name);
+        name.setText(m.name);
+        TextView phone = findViewById(R.id.phone);
+        phone.setText(m.phone);
+        TextView email = findViewById(R.id.email);
+        email.setText(m.email);
+        TextView addr = findViewById(R.id.addr);
+        addr.setText(m.addr);
+        findViewById(R.id.callBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.dialBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.smsBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.emailBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.albumBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.movieBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.mapBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.musicBtn).setOnClickListener(
+                (v)->{}
+        );
+        findViewById(R.id.updateBtn).setOnClickListener(
+                (v)->{
+                    Intent intent2 = new Intent(_this, MemberUpdate.class);
+                    intent2.putExtra("spec",
+                            m.seq+","+
+                                m.name+","+
+                                m.pw+","+
+                                m.email+","+
+                                m.phone+","+
+                                m.addr+","+
+                                m.photo
+                    );
+                    startActivity(intent2);
+
+                });
+        findViewById(R.id.listBtn).setOnClickListener(
+                (v)->{
+                    startActivity(new Intent(_this, MemberList.class));
+                }
+        );
+
+
+    }
+    private class DetailQuery extends QueryFactory{
+        SQLiteOpenHelper helper;
+        public DetailQuery(Context _this) {
+            super(_this);
+            helper = new SQLiteHelper(_this);
+        }
+
+        @Override
+        public SQLiteDatabase getDatabase() {
+            return helper.getReadableDatabase();
+        }
+    }
+    private class ItemDetail extends DetailQuery{
+        String id;
+        public ItemDetail(Context _this) {
+            super(_this);
+        }
+        public Member get(){
+            Member m = null;
+            Cursor c = getDatabase()
+                    .rawQuery(
+                        String.format(
+                                " SELECT * FROM %s WHERE %s LIKE '%s' "
+                                ,MEMBERS, MSEQ, id),null);
+
+                if(c != null && c.moveToNext()){
+                    m = new Member();
+                    m.seq = Integer.parseInt(c.getString(c.getColumnIndex(MSEQ)));
+                    m.name = c.getString(c.getColumnIndex(MNAME));
+                    m.pw = c.getString(c.getColumnIndex(MPW));
+                    m.email = c.getString(c.getColumnIndex(MEMAIL));
+                    m.phone = c.getString(c.getColumnIndex(MPHONE));
+                    m.addr = c.getString(c.getColumnIndex(MEMAIL));
+                    m.photo = c.getString(c.getColumnIndex(MPHOTO));
+                }
+            return m;
+        }
     }
 }
--- app/src/main/java/app/demo/com/contacts/MemberList.java
+++ app/src/main/java/app/demo/com/contacts/MemberList.java
@@ -1,6 +1,7 @@
 package app.demo.com.contacts;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,13 +11,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
+import static app.demo.com.contacts.Index.*;
 
 public class MemberList extends AppCompatActivity {
 
@@ -30,13 +32,24 @@ public class MemberList extends AppCompatActivity {
 
 
         memberList.setAdapter(
-              new MemberAdapter(_this, (ArrayList<Index.Member>)new Index.ISupplier() {
+              new MemberAdapter(_this, (ArrayList<Member>)new ISupplier() {
                   @Override
                   public Object get() {
                       return query.get();
                   }
               }.get())
         );
+        memberList.setOnItemClickListener(
+                (AdapterView<?> p, View v, int i, long l)->{
+                    Member m = (Member)memberList.getItemAtPosition(i);
+                    Toast.makeText(_this,m.name,Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(_this, MemberDetail.class);
+                    intent.putExtra("seq", m.seq);
+                    startActivity(intent);
+                });
+
+        // memberList.setOnItemLongClickListener(()->{});
+
 
     }
     private class MemberListQuery extends Index.QueryFactory{
@@ -57,22 +70,22 @@ public class MemberList extends AppCompatActivity {
             super(_this);
         }
 
-        public ArrayList<Index.Member> get(){
-            ArrayList<Index.Member> list = new ArrayList<>();
+        public ArrayList<Member> get(){
+            ArrayList<Member> list = new ArrayList<>();
             Cursor c = this.getDatabase().rawQuery(
                      " SELECT * FROM MEMBER ", null
             );
-            Index.Member m = null;
+            Member m = null;
             if(c != null){
                 while(c.moveToNext()){
-                    m = new Index.Member();
-                    m.seq = Integer.parseInt(c.getString(c.getColumnIndex(Index.MSEQ)));
-                    m.name = c.getString(c.getColumnIndex(Index.MNAME));
-                    m.pw = c.getString(c.getColumnIndex(Index.MPW));
-                    m.email = c.getString(c.getColumnIndex(Index.MEMAIL));
-                    m.phone = c.getString(c.getColumnIndex(Index.MPHONE));
-                    m.addr = c.getString(c.getColumnIndex(Index.MEMAIL));
-                    m.photo = c.getString(c.getColumnIndex(Index.MPHOTO));
+                    m = new Member();
+                    m.seq = Integer.parseInt(c.getString(c.getColumnIndex(MSEQ)));
+                    m.name = c.getString(c.getColumnIndex(MNAME));
+                    m.pw = c.getString(c.getColumnIndex(MPW));
+                    m.email = c.getString(c.getColumnIndex(MEMAIL));
+                    m.phone = c.getString(c.getColumnIndex(MPHONE));
+                    m.addr = c.getString(c.getColumnIndex(MEMAIL));
+                    m.photo = c.getString(c.getColumnIndex(MPHOTO));
                     Log.d("멤버정보 :  ",m.name);
                     list.add(m);
 
@@ -87,11 +100,11 @@ public class MemberList extends AppCompatActivity {
 
     }
     private class MemberAdapter extends BaseAdapter{
-        ArrayList<Index.Member> list;
+        ArrayList<Member> list;
         LayoutInflater inflater;
         Context _this;
 
-        public MemberAdapter(Context _this,ArrayList<Index.Member> list ) {
+        public MemberAdapter(Context _this,ArrayList<Member> list ) {
             this.list = list;
             this._this = _this;
             this.inflater = LayoutInflater.from(_this);
@@ -157,11 +170,11 @@ public class MemberList extends AppCompatActivity {
         TextView name, phone;
     }
 
-    private class MemberPhotoQuery extends Index.QueryFactory{
+    private class MemberPhotoQuery extends QueryFactory{
         SQLiteOpenHelper helper;
         public MemberPhotoQuery(Context _this) {
             super(_this);
-            helper = new Index.SQLiteHelper(_this);
+            helper = new SQLiteHelper(_this);
         }
 
         @Override
@@ -180,14 +193,14 @@ public class MemberList extends AppCompatActivity {
                     .rawQuery(String.format(
                             " SELECT %s FROM %s "
                             +" WHERE %s LIKE '%s' "
-                            ,Index.MPHOTO, Index.MEMBERS,
-                            Index.MSEQ, seq), null
+                            ,MPHOTO, MEMBERS,
+                            MSEQ, seq), null
                     );
 
             String result = "";
             if(c != null){
                 if(c.moveToNext()){
-                    result = c.getString(c.getColumnIndex(Index.MPHOTO));
+                    result = c.getString(c.getColumnIndex(MPHOTO));
                 }
             }
             return result;
